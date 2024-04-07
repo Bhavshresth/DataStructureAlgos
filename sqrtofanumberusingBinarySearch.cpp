6f1787ec0bfdc52085c9d584b7bcae755af7bad5
Bhavshresth Pattanayak <117659396+Bhavshresth@users.noreply.github.com>
2024-04-07 16:25:15 +0530

    Update sqrtofanumberusingBinarySearch.cpp

--- sqrtofanumberusingBinarySearch.cpp
+++ sqrtofanumberusingBinarySearch.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int BS(int n){
 	int first=1,last=n/2+1,ans;
 	while(first<=last){
-		int mid=(first+last)/2;
+		int mid=(first+last)/2;//start+(last-start)/2;
 		if(mid*mid==n)	return mid;
 		else if(mid*mid>n)	last=mid-1;
 		else{
