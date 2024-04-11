c11ff5704f2ea671b4317e28059d50c983cdae94
Bhavshresth Pattanayak <117659396+Bhavshresth@users.noreply.github.com>
2024-04-11 12:04:28 +0530

    Update BitOperations.cpp

--- BitOperations.cpp
+++ BitOperations.cpp
@@ -19,14 +19,17 @@ int main(){
 	cout<<"Brute Solution O(N^2) "<<bruteXOR(arr,n)<<endl;
 	
 	for(int i=0;i<32;i++){
+		//i=0 means we are concerned about 0th bit from the last
+		//i=1 meams we are concered about 1 indexed bit frim last (2nd bit from last)
 		int zeroCount=0,oneCount=0;
 		for(int j=0;j<n;j++){
-			if(arr[j]&1) oneCount++;
+			//How do we find last bit 1/0?
+			if(arr[j]&1) oneCount++;//not zero
 			else zeroCount++;
 			//a[j]=a[j]/2;
 			arr[j]=arr[j]>>1;
 		}
-		sum+=(oneCount)*zeroCount*base;
+		sum+=(oneCount)*zeroCount*base;//because last bit is contribution 1 ,2nd last is contribution 2,4,8
 		base*=2;
 		//base=base<<1;
 	}
